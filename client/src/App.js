36bf6ffa47eea905bbe7cdb5e4ed9a50445ef7e4
Ariel Gerstein <ariel.ger@hotmail.com>
2018-10-05 22:59:50 -0300

    Refactor routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,36 +4,12 @@ import SignUp from "./account/SignUp";
 import LogIn from "./account/LogIn";
 import BookmarksList from "./bookmarks/BookmarksList";
 
-const ConditionalRoute = ({
-  shouldRender,
-  redirect,
-  render,
-  component: Component,
-  ...rest
-}) => (
-  <Route
-    {...rest}
-    render={props =>
-      shouldRender ? (
-        render ? (
-          render(props)
-        ) : (
-          <Component {...props} />
-        )
-      ) : (
-        <Redirect to={redirect} />
-      )
-    }
-  />
-);
-
 class App extends Component {
   state = {
     userToken: sessionStorage.getItem("jwtToken") || ""
   };
 
   setUserToken = userToken => {
-    console.log("userToken", userToken);
     sessionStorage.setItem("jwtToken", userToken);
     this.setState({ userToken });
   };
@@ -44,24 +20,25 @@ class App extends Component {
     return (
       <div className="App">
         <Switch>
-          <ConditionalRoute
-            shouldRender={!isAuthenticated}
-            path="/signup"
-            redirect="/"
-            render={() => <SignUp setUserToken={this.setUserToken} />}
-          />
-          <ConditionalRoute
-            shouldRender={!isAuthenticated}
-            path="/login"
-            redirect="/"
-            render={() => <LogIn setUserToken={this.setUserToken} />}
-          />
-          <ConditionalRoute
-            shouldRender={isAuthenticated}
-            redirect="/login"
+          {!isAuthenticated && (
+            <Switch>
+              <Route
+                path="/signup"
+                render={() => <SignUp setUserToken={this.setUserToken} />}
+              />
+              <Route
+                path="/login"
+                render={() => <LogIn setUserToken={this.setUserToken} />}
+              />
+              <Redirect to="/login" />
+            </Switch>
+          )}
+          <Route
+            path="/"
             exact
             render={() => <BookmarksList userToken={this.state.userToken} />}
           />
+          <Redirect to="/" />
         </Switch>
       </div>
     );
