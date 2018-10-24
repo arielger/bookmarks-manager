1365c8e6b001bfcb650362fbf11eb39811335cac
Ariel Gerstein <ariel.ger@hotmail.com>
2018-10-23 21:44:49 -0300

    Show error when log in fail, add show password toggle

--- client/src/scenes/LogIn/LogIn.js
+++ client/src/scenes/LogIn/LogIn.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Form, Icon, Input, Button } from "antd";
+import { Form, Icon, Input, Button, Checkbox, message } from "antd";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { users as usersApi } from "../../api";
@@ -8,29 +8,66 @@ import { users as usersApi } from "../../api";
 const FormWrapper = styled.div`
   max-width: 320px;
   margin: 60px auto 0;
+
+  .password-container {
+    margin-bottom: 4px;
+  }
+
+  .show-password {
+    margin-bottom: 24px;
+  }
+`;
+
+const LinksContainer = styled.div`
+  margin-top: 16px;
+  text-align: center;
 `;
 
 class LogIn extends Component {
+  state = {
+    isLoading: false,
+    showPassword: false
+  };
+
   static propTypes = {
     setUserToken: PropTypes.func.isRequired
   };
 
   handleSubmit = e => {
+    const { form } = this.props;
     e.preventDefault();
-    this.props.form.validateFields((err, values) => {
+
+    form.validateFields((err, values) => {
       if (!err) {
+        this.setState({ isLoading: true });
         usersApi
           .login(values)
           .then(({ data: { token } }) => {
             this.props.setUserToken(token);
+            this.setState({ isLoading: false });
           })
-          .catch(error => console.error("Error:", error));
+          .catch(error => {
+            message.error(
+              "Unable to login. Please check your email and password and try again."
+            );
+            form.setFieldsValue({
+              password: ""
+            });
+            this.setState({ isLoading: false });
+          });
       }
     });
   };
 
+  toggleShowPassword = () => {
+    this.setState(prevState => ({
+      showPassword: !prevState.showPassword
+    }));
+  };
+
   render() {
     const { getFieldDecorator } = this.props.form;
+    const { isLoading, showPassword } = this.state;
 
     return (
       <FormWrapper>
@@ -56,25 +93,37 @@ class LogIn extends Component {
               />
             )}
           </Form.Item>
-          <Form.Item>
+          <Form.Item className="password-container">
             {getFieldDecorator("password", {
               rules: [{ required: true, message: "Please input your password" }]
             })(
               <Input
                 prefix={<Icon type="lock" />}
-                type="password"
+                type={showPassword ? "text" : "password"}
                 placeholder="Password"
                 size="large"
               />
             )}
           </Form.Item>
+          <Checkbox
+            className="show-password"
+            onChange={this.toggleShowPassword}
+          >
+            Show password
+          </Checkbox>
           <Form.Item>
-            <Button type="primary" htmlType="submit" size="large" block>
+            <Button
+              type="primary"
+              htmlType="submit"
+              size="large"
+              loading={isLoading}
+              block
+            >
               Log In
             </Button>
-            <p>
-              Or <Link to="/signup">Sign up</Link>
-            </p>
+            <LinksContainer>
+              <Link to="/signup">Sign up ›</Link>
+            </LinksContainer>
           </Form.Item>
         </Form>
       </FormWrapper>
