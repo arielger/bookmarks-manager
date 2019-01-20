0be73eb93c3d8ed9ed0e9f704d3408689d387c6e
Ariel Gerstein <ariel.ger@hotmail.com>
2019-01-20 17:27:57 -0300

    Update env variable from SERVER_PORT to PORT

--- src/server/app.js
+++ src/server/app.js
@@ -19,7 +19,7 @@ router.set(app);
 
 // Prevent bounding server to port in test env
 if (process.env.NODE_ENV !== "test") {
-  const port = process.env.SERVER_PORT || 5000;
+  const port = process.env.PORT || 5000;
 
   app.listen(port, () =>
     // eslint-disable-next-line no-console
