75669417b941dfda80abde1876d5226e394554fe
Ariel Gerstein <ariel.ger@hotmail.com>
2019-01-23 23:26:17 -0300

    Add client/.env file

--- client/src/api.js
+++ client/src/api.js
@@ -3,10 +3,6 @@ import axios from "axios";
 const BASE_URL =
   process.env.NODE_ENV === "development" ? "" : process.env.REACT_APP_API_URL;
 
-console.log("process.env.REACT_APP_API_URL", process.env.REACT_APP_API_URL);
-console.log("process.env.NODE_ENV", process.env.NODE_ENV);
-console.log("BASE_URL", BASE_URL);
-
 axios.defaults.baseURL = BASE_URL;
 axios.defaults.headers.common["Content-Type"] = "application/json";
 
