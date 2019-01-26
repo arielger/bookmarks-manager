a80c6d296da2e3c757f99d3c03769974ff9936d8
Ariel Gerstein <ariel.ger@hotmail.com>
2019-01-26 12:20:45 -0300

    Get default title and description from pages

--- src/server/components/bookmarks/controller.js
+++ src/server/components/bookmarks/controller.js
@@ -1,5 +1,7 @@
 const R = require("ramda");
 const Joi = require("joi");
+const cheerio = require("cheerio");
+const axios = require("axios");
 const sequelizeToJoi = require("@revolttv/sequelize-to-joi").default;
 const { isURL } = require("validator");
 const bookmarkService = require("./service");
@@ -9,7 +11,25 @@ const { Bookmark } = db;
 
 const bookmarkValidator = sequelizeToJoi(Bookmark);
 
-// @todo: Add PUT
+// Default to page title or description if they are not defined
+const getBookmarkDefaults = async bookmark => {
+  if (bookmark.title && bookmark.description) return bookmark;
+
+  try {
+    const { data } = await axios.get(bookmark.url);
+
+    const $ = cheerio.load(data);
+    return R.pipe(
+      R.unless(R.has("title"), R.assoc("title", $("title").text())),
+      R.unless(
+        R.has("description"),
+        R.assoc("description", $("meta[name=description]").attr("content"))
+      )
+    )(bookmark);
+  } catch (error) {
+    return bookmark;
+  }
+};
 
 // @todo: Implement pagination, filtering, sorting
 const getBookmarks = (req, res) => {
@@ -23,16 +43,20 @@ const getBookmark = (req, res) => {
     .getByIdFromUser(req.userId, bookmarkId)
     .then(bookmark => {
       if (!bookmark) {
-        return res
-          .status(404)
-          .send({ error: "Bookmark not found for the user" });
+        return res.status(404).send({
+          error: "Bookmark not found for the user"
+        });
       }
       return res.status(200).send(bookmark);
     })
-    .catch(err => res.status(500).send({ error: err }));
+    .catch(err =>
+      res.status(500).send({
+        error: err
+      })
+    );
 };
 
-const addBookmark = (req, res) => {
+const addBookmark = async (req, res) => {
   const bookmarkData = R.pick(["url", "title", "description"], req.body);
 
   // @todo: Move joi validation to middleware
@@ -74,11 +98,7 @@ const addBookmark = (req, res) => {
   }
 
   return bookmarkService
-    .add(req.userId, {
-      url: req.body.url,
-      title: req.body.title,
-      description: req.body.description
-    })
+    .add(req.userId, await getBookmarkDefaults(bookmarkData))
     .then(data => res.status(200).send(data));
 };
 
@@ -94,13 +114,17 @@ const updateBookmark = (req, res) => {
     .updateByIdFromUser(req.userId, bookmarkId, updatedData)
     .then(bookmark => {
       if (!bookmark) {
-        return res
-          .status(404)
-          .send({ error: "Bookmark not found for the user" });
+        return res.status(404).send({
+          error: "Bookmark not found for the user"
+        });
       }
       return res.status(200).send(bookmark);
     })
-    .catch(err => res.status(500).send({ error: err }));
+    .catch(err =>
+      res.status(500).send({
+        error: err
+      })
+    );
 };
 
 const deleteBookmark = (req, res) => {
@@ -110,13 +134,17 @@ const deleteBookmark = (req, res) => {
     .deleteFromUser(req.userId, bookmarkId)
     .then(bookmark => {
       if (!bookmark) {
-        return res
-          .status(404)
-          .send({ error: "Bookmark not found for the user" });
+        return res.status(404).send({
+          error: "Bookmark not found for the user"
+        });
       }
       return res.status(200).send(bookmark);
     })
-    .catch(err => res.status(500).send({ error: err }));
+    .catch(err =>
+      res.status(500).send({
+        error: err
+      })
+    );
 };
 
 module.exports = {
