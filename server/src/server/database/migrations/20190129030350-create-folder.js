cae56da49d5572efdb2aa29d887eeebe6768966d
Ariel Gerstein <ariel.ger@hotmail.com>
2019-01-29 17:19:48 -0300

    Add association between bookmarks and folders

--- server/src/server/database/migrations/20190129030350-create-folder.js
+++ server/src/server/database/migrations/20190129030350-create-folder.js
@@ -1,31 +1,43 @@
 module.exports = {
   up: (queryInterface, Sequelize) =>
-    queryInterface.createTable("Folders", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      title: {
-        type: Sequelize.STRING,
-        notEmpty: true
-      },
-      userId: {
-        type: Sequelize.INTEGER,
+    Promise.all([
+      queryInterface.createTable("Folders", {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        title: {
+          type: Sequelize.STRING,
+          notEmpty: true
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+          references: {
+            model: "Users",
+            key: "id"
+          }
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      }),
+      queryInterface.addColumn("Bookmarks", "folderId", Sequelize.INTEGER, {
         references: {
           model: "Users",
           key: "id"
         }
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    }),
-  down: queryInterface => queryInterface.dropTable("Folders")
+      })
+    ]),
+  down: queryInterface =>
+    Promise.all([
+      queryInterface.dropTable("Folders"),
+      queryInterface.removeColumn("Bookmarks", "folderId")
+    ])
 };
--- server/src/server/database/models/bookmark.js
+++ server/src/server/database/models/bookmark.js
@@ -26,6 +26,7 @@ module.exports = (sequelize, DataTypes) => {
   });
   Bookmark.associate = models => {
     Bookmark.belongsTo(models.User, { foreignKey: "userId" });
+    Bookmark.belongsTo(models.Folder, { foreignKey: "folderId" });
   };
   return Bookmark;
 };
